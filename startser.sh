916dff0294bcd2530da9e776250a47ff94efc509
ynmr <46440554+ynmr@users.noreply.github.com>
2019-01-07 14:32:38 +0530

    Add files via upload

---
+++ startser.sh
@@ -0,0 +1,3 @@
+/local/mnt/servers/tomcat-8.5.4/bin/startup.sh
+/local/mnt/servers/solr-6.1.0/bin/solr start 
+
---
+++ stopser.sh
@@ -0,0 +1,3 @@
+/local/mnt/servers/tomcat-8.5.4/bin/shutdown.sh
+/local/mnt/servers/solr-6.1.0/bin/solr stop 
+
